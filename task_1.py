4196724f78220acd923eda8e8736f4c9ed4076b0
unkxll <valiullovtwink@mail.ru>
2026-09-06 16:52:47 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,1 +1,22 @@
+# Лабораторная №1: Первичная инициализация
+# Курс: Основы теории систем
+# Студент: Валиуллов Рустам Маратович
 
+def get_system_info():
+    """
+    Эта функция должна вернуть словарь с информацией о вашей "системе".
+    """
+    # TODO: Заполните словарь вашими реальными данными
+    system_info = {
+        "student_name": "Валиуллов Рустам Маратович",
+        "academic_group": "ИВТИИбд-13",
+        "github_link": "https://github.com/unkxll/ots_practice_2026-/tree/main"
+    }
+    return system_info
+
+# Вывод информации для проверки
+if __name__ == "__main__":
+    info = get_system_info()
+    print("Информация о системе:")
+    for key, value in info.items():
+        print(f"- {key}: {value}")
